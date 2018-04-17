e2880606a4647a0f77bd25a458eaca71b9c73613
Arsalan Bashir <afterclass.dev@gmail.com>
2018-04-17 20:27:58 +0400

    Configured Firebase

--- app.py
+++ app.py
@@ -2,15 +2,20 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import jsonify
+from firebase_admin import db
+import config
 
 app = Flask(__name__)
+
+cred = credentials.Certificate(config.firebase['serviceAccount'])
+firebase_admin.initialize_app(cred, config.firebase)
 
 @app.route('/', methods=["GET", "POST"])
 def home():
     if request.method == "GET":
         return render_template("home.html")
     else:
-        Id = "some Id here"
+        Id = db.reference('/jots').push()
         return jsonify({"Message": "Success", "Identifier": Id})
 
 @app.route('/jot/<Id>', methods=["GET", "POST"])
---
+++ config-sample.py
@@ -0,0 +1,15 @@
+import os
+
+current_dir = os.path.dirname(os.path.realpath(__file__))
+
+firebase = {
+    "apiKey": "______",
+    "authDomain": "______",
+    "databaseURL": "______",
+    "projectId": "______",
+    "storageBucket": "______",
+    "messagingSenderId": "______",
+    "serviceAccount": current_dir + "/service.json"
+}
+
+appKey = "some-key-here"
