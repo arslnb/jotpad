c951429c8cb83b5624dc3b57a55b5e7028917369
Arsalan Bashir <afterclass.dev@gmail.com>
2018-04-17 20:01:59 +0400

    Added simple POST request using XMLHttpRequest

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template("home.html")
+    if request.method == "GET":
+        return render_template("home.html")
+    else:
+        Id = "some Id here"
+        return jsonify({"Message": "Success", "Identifier": Id})
+
+@app.route('/jot/<Id>', methods=["GET", "POST"])
+def notepad(Id):
+    return Id
