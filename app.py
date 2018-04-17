9673f14ff31f2ac4b521b3d1d89445a52f94cce6
Arsalan Bashir <arsalan.b4@gmail.com>
2018-04-17 20:36:15 +0400

    Added redirects

--- app.py
+++ app.py
@@ -2,20 +2,22 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import jsonify
+from firebase_admin import initialize_app
 from firebase_admin import db
+from firebase_admin import credentials
 import config
 
 app = Flask(__name__)
 
 cred = credentials.Certificate(config.firebase['serviceAccount'])
-firebase_admin.initialize_app(cred, config.firebase)
+initialize_app(cred, config.firebase)
 
 @app.route('/', methods=["GET", "POST"])
 def home():
     if request.method == "GET":
         return render_template("home.html")
     else:
-        Id = db.reference('/jots').push()
+        Id = db.reference('/jots').push().key
         return jsonify({"Message": "Success", "Identifier": Id})
 
 @app.route('/jot/<Id>', methods=["GET", "POST"])
